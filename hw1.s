ae17812f73127d14342dd037030f174419e88b9f
Lindsey Michie <lmichie@nd.edu>
2021-09-06 20:19:18 -0400

    initialized values

--- hw1.s
+++ hw1.s
@@ -3,6 +3,8 @@
 		ldi r10, 10
 		ldi r20,20		//load 20 to r20
 		ldi r7, 1		//load 1 to r7
+		ldi r2, 18		//set x to 18
+		ldi r3, 0		//set y to 0
 loop:	sub r8, r1, r10
 		bz r8, end
 		sub r9, r2,r20	//compare x to 20
@@ -10,7 +12,7 @@ loop:	sub r8, r1, r10
 		bz  r9, q		//if x==20, jump to q:y = y + 1 and store
 		sub r3, r3,r7	//else y = y - 1
 		st  r3, r10		//store in m[10,000]
-		b	always
+		br	always
 q:		add r3, r3, r7
 		st  r3, r10,30	//store in m[20,000]
 always:	add r2, r2, r7	//x = x + 1
